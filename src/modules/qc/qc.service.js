8c631a11b0101f1b03657fe3941a20ddffdf9db9
rajbhkun <kunal.rajbhoj@dspim.com>
2022-01-15 11:45:42 +0530

    minor fix

--- src/modules/qc/qc.service.js
+++ src/modules/qc/qc.service.js
@@ -2,7 +2,8 @@ import {vwJobList} from '../../models/vw_job_list'
 
 const getJobsPendingForQC = mst_service_location_id => vwJobList.findAll({
 	where: {mst_service_location_id,
-		mst_action_status: 'Pending For QC'}
+		mst_action_status: 'Pending For QC'},
+	raw: true
 })
 
 export {
